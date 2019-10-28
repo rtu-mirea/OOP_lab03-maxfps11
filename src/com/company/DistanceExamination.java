907c3fd5b3ca81ef20b117b31e2a5c4ae839d106
Mikhail <mikhail.code@icloud.com>
2019-10-28 17:10:35 +0300

    Update

--- src/com/company/DistanceExamination.java
+++ src/com/company/DistanceExamination.java
@@ -1,6 +1,8 @@
 package com.company;
 
 
+import com.sun.jdi.Value;
+
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -28,26 +30,38 @@ class Question {
     boolean isCorrect(String answer) {
         return this.m_answer.equals(answer);
     }
+
+    void print() {
+        System.out.println("Текст вопроса: " + this.m_text + "\nОтвет: " + this.m_answer);
+    }
 }
 
-class Result {
-    //...
+class ExamSettings {
+    private Map<String, Integer> parameters = new HashMap<String, Integer>();
+
+    Integer getParameters(String key) {
+        return this.parameters.get(key);
+    }
+
+    void setParameters(String key, Integer value) {
+        this.parameters.put(key, value);
+    }
 }
 
 public class DistanceExamination {
     // loading data
+    private static ExamSettings m_settings = new ExamSettings();
     private static List<User> m_users = new LinkedList<User>();
-    private static List<Question> m_questions;
+    private static List<Question> m_questions = new LinkedList<Question>();
 
     // global vars
     private static Scanner  in              = new Scanner(System.in);
     private static User     m_currentUser   = null;
     private static boolean  unknownUser     = true;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         if (loadData()) {
-            m_users.add(new Admin());
-            m_users.add(new Student("Михаил", "max", "max"));
+            m_users.add(new Admin(m_questions, m_users));
 
             while (true) {
                 while (unknownUser) {
@@ -66,6 +80,7 @@ public class DistanceExamination {
                         System.out.println("Unknown user.");
                 }
 
+                assert m_currentUser != null;
                 m_currentUser.consoleInterface();
 
                 m_currentUser = null;
@@ -87,23 +102,88 @@ public class DistanceExamination {
     }
 
     private static boolean loadData() throws IOException {
-        FileReader reader = new FileReader("students.txt");
-        String textFile = "";
+        FileReader reader = new FileReader("/Users/maxfps11/IdeaProjects/ThirdLaboratoryWork/src/com/company/examInfo.txt");
+        StringBuilder examInfoFile = new StringBuilder();
         int c;
 
         while ((c = reader.read()) > 0)
-            textFile += (char)c;
+            examInfoFile.append((char) c);
 
-        String regex = "[a-zA-Z[а-яА-Я]]{2,}\\G\\s\\b[a-zA-Z[а-яА-Я]]{2,}\\G\\s\\b[a-zA-Z[а-яА-Я]]{2,}\\b";
+        String parameterLineRegex = "\\b[a-zA-Z]+\\b\\s*=\\s*\\b\\d+\\b";
+        String parameterNameRegex = "\\b[a-zA-Z]+\\b";
+        String parameterValueRegex = "\\b\\d+\\b";
 
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(textFile);
+        Pattern pattern = Pattern.compile(parameterLineRegex);
+        Matcher parameterLineMatcher = pattern.matcher(examInfoFile.toString());
 
-        while (matcher.find()) {
-            System.out.println(textFile.substring(matcher.start(), matcher.end()));
+        Pattern parameterValuePattern = Pattern.compile(parameterValueRegex);
+        Pattern parameterNamePattern = Pattern.compile(parameterNameRegex);
+
+        Matcher parameterValueMatcher;
+        Matcher parameterNameMatcher;
+
+        String buffer = "";
+
+        while (parameterLineMatcher.find()) {
+            buffer = examInfoFile.substring(parameterLineMatcher.start(), parameterLineMatcher.end());
+
+            parameterNameMatcher = parameterNamePattern.matcher(buffer);
+            parameterValueMatcher = parameterValuePattern.matcher(buffer);
+
+            while (parameterNameMatcher.find() && parameterValueMatcher.find()) {
+                m_settings.setParameters(buffer.substring(parameterNameMatcher.start(), parameterNameMatcher.end()),
+                        Integer.parseInt(buffer.substring(parameterValueMatcher.start(), parameterValueMatcher.end())));
+            }
         }
 
-        return false;
+        reader = new FileReader("/Users/maxfps11/IdeaProjects/ThirdLaboratoryWork/src/com/company/students.txt");
+        StringBuilder studentsFile = new StringBuilder();
+
+        while ((c = reader.read()) > 0)
+            studentsFile.append((char) c);
+
+        String nameRegex = "name:[a-zA-Z[а-яА-Я]]{2,}\\s\\b[a-zA-Z[а-яА-Я]]{2,}\\s\\b[a-zA-Z[а-яА-Я]]{2,}\\b";
+        String loginRegex = "login:([a-zA-Z]|[0-9]){3,}";
+        String passRegex = "password:([a-zA-Z]|[0-9]){3,}";
+
+        pattern = Pattern.compile(nameRegex);
+        Matcher nameMatcher = pattern.matcher(studentsFile.toString());
+
+        pattern = Pattern.compile(loginRegex);
+        Matcher loginMatcher = pattern.matcher(studentsFile.toString());
+
+        pattern = Pattern.compile(passRegex);
+        Matcher passMatcher = pattern.matcher(studentsFile.toString());
+
+        while (nameMatcher.find() && loginMatcher.find() && passMatcher.find()) {
+            m_users.add(new Student(studentsFile.substring(nameMatcher.start(), nameMatcher.end()).replace("name:", ""),
+                    studentsFile.substring(loginMatcher.start(), loginMatcher.end()).replace("login:", ""),
+                    studentsFile.substring(passMatcher.start(), passMatcher.end()).replace("password:", "")));
+        }
+        reader = new FileReader("/Users/maxfps11/IdeaProjects/ThirdLaboratoryWork/src/com/company/questions.txt");
+        StringBuilder questionsFile = new StringBuilder();
+
+        while ((c = reader.read()) > 0)
+            questionsFile.append((char) c);
+
+        String quesRegex = "question:.+";
+        String answerRegex = "answer:.+";
+
+        pattern = Pattern.compile(quesRegex);
+        Matcher quesMatcher = pattern.matcher(questionsFile.toString());
+
+        pattern = Pattern.compile(answerRegex);
+        Matcher answerMatcher = pattern.matcher(questionsFile.toString());
+
+        while (quesMatcher.find() && answerMatcher.find()) {
+            m_questions.add(new Question(
+                    questionsFile.substring(quesMatcher.start(), quesMatcher.end()).replace(
+                            "question:", "").replace(
+                                    "\\n", "\n"),
+                    questionsFile.substring(answerMatcher.start(), answerMatcher.end()).replace(
+                            "answer:", "")));
+        }
+        return studentsFile.length() > 0 && questionsFile.length() > 0;
     }
 
     private boolean saveData() {
